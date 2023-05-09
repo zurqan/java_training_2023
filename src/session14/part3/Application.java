2cd0d3eb5acb37ddcff16fe0a05bb9a6555f6772
zurqan <zurqan@gmail.com>
2023-05-09 13:06:21 +0300

    session 14

--- src/session14/part3/Application.java
+++ src/session14/part3/Application.java
@@ -1,5 +1,8 @@
 package session14.part3;
 
+import session9.part2.ExpandedArray;
+
+import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
@@ -15,8 +18,31 @@ public class Application {
         Optional<Integer> max2 = max( );
 
         System.out.println("max2 = " + max2);
+
+        Integer sumR = reduceR(0, e -> acc -> e + acc, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println("sumR = " + sumR);
+
+        ExpandedArray<String> stringExpandedArray = toList("Ahmad","Mohammad");
+
+        stringExpandedArray.forEach(System.out::println);
+        ExpandedArray<Integer> integerExpandedArray = toList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        integerExpandedArray.forEach(System.out::println);
     }
 
+    //toList->List
+    //grouping(keyExtractorFunction,valueExtractorFunction)->Map<Key,List<V>>
+
+    //Students -> List<Integer> using streams
+//     -> gropuing student by age <20 using streams
+
+    public static <T> ExpandedArray<T> toList(T...data){
+
+        return reduceL(new ExpandedArray<T>(),
+                acc -> e -> {
+                    acc.addElement(e);
+                    return acc;
+                }, data);
+    }
 //    public static int max(Integer... data){
 //        return reduceL(0, acc -> e -> acc > e?acc:e, data);//TODO need to be revisited
 //    }
@@ -34,7 +60,14 @@ public class Application {
 
 
     public static <U,T> U reduceR(U seed,Function<T,Function<U,U>> accFunction,T... data){
-
+        if(data==null || data.length==0){
+            return seed;
+        }
+        U result = seed;
+        for (int i = data.length - 1; i >= 0; i--) {
+            result=accFunction.apply(data[i]).apply(result);
+        }
+        return result;
     }
     public static <U,T> U reduceL(U seed, Function<U,Function<T,U>> accFunction, T... data){
 
